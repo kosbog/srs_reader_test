4301749ae1d9e485c938028afe13cd9430488193
kosbog <badykos@gmail.com>
2025-02-06 13:39:37 +0200

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center gap-2">
-      <Link href="/book"><span className="dark:text-light-text text-button-text">Read</span></Link>
+      <Link href="/book">
+        <h1 className="dark:text-light-text text-button-text text-3xl">
+          Read me
+        </h1>
+      </Link>
     </main>
   );
 }
